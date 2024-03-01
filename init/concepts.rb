2565460341835effaf7091f1a981ae626b806891
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-03-01 17:14:29 -0300

    inicio com a live do américo

---
+++ init/concepts.rb
@@ -0,0 +1,74 @@
+# Aula feita no https://try.ruby-lang.org/ 
+
+
+
+# 3.times do
+#     print "Hello, World! "
+# end
+
+# converte um numero para string
+# puts 40.to_s.reverse
+
+# acha o maior
+# [12, 47, 35].max
+# print [12,3321,13].min
+
+# salva os numeros
+# numeros = [23,212,31]
+# print numeros
+
+# ordenar os numeros 
+# print numeros.sort!
+
+# Salva um texto
+
+# poema = "Minha torrada voou da minha mão
+# E minha torrada se foi para a lua.
+# Mas quando eu vejo isso na televisão,
+# Colocando nossa bandeira no cometa Halley,
+# Mais ainda eu quero come-la."
+
+# print poema
+
+# troca torrada por manga em um texto 
+# print poema.gsub("torrada", "manga")
+
+# inverte um texto
+# print poema.reverse
+
+# transforma tudo em uma linha 
+# print poema.lines.reverse
+
+# encadeamento
+
+# print puts poema.lines.reverse.join
+
+# dicionario em ruby 
+# livros = {}
+
+# livros["O Arco-Íris da Gravidade"] = :esplendido
+# livros["Até o Fim"]  = :lixo
+# livros["Cores Vivas"] = :mediocre
+
+# ver o resultado unitario
+# puts livros["O Arco-Íris da Gravidade"]
+# completo
+# puts livros
+# var tamanho
+# puts livros.length
+
+# ver todos 
+# puts livros.keys
+# como array
+# print livros.keys
+
+# Hash em ruby 
+# classificacoes = Hash.new {0}
+
+# livros.values.each { |classificacao|
+#   classificacoes[classificacao] += 1
+# }
+
+# puts classificacoes
+
+# parei em hash 
