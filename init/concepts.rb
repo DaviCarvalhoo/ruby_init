3371d09fccb0c4211b6b2baa7adbb04527e36458
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-03-07 15:43:29 -0300

    finish

--- init/concepts.rb
+++ init/concepts.rb
@@ -1,4 +1,4 @@
-# Aula feita no https://try.ruby-lang.org/ 
+# Aula feita no https://try.ruby-lang.org/
 
 
 
@@ -17,7 +17,7 @@
 # numeros = [23,212,31]
 # print numeros
 
-# ordenar os numeros 
+# ordenar os numeros
 # print numeros.sort!
 
 # Salva um texto
@@ -30,20 +30,20 @@
 
 # print poema
 
-# troca torrada por manga em um texto 
+# troca torrada por manga em um texto
 # print poema.gsub("torrada", "manga")
 
 # inverte um texto
 # print poema.reverse
 
-# transforma tudo em uma linha 
+# transforma tudo em uma linha
 # print poema.lines.reverse
 
 # encadeamento
 
 # print puts poema.lines.reverse.join
 
-# dicionario em ruby 
+# dicionario em ruby
 # livros = {}
 
 # livros["O Arco-Íris da Gravidade"] = :esplendido
@@ -57,12 +57,12 @@
 # var tamanho
 # puts livros.length
 
-# ver todos 
+# ver todos
 # puts livros.keys
 # como array
 # print livros.keys
 
-# Hash em ruby 
+# Hash em ruby
 # classificacoes = Hash.new {0}
 
 # livros.values.each { |classificacao|
@@ -71,4 +71,67 @@
 
 # puts classificacoes
 
-# parei em hash 
+# Hash ruby
+
+# classificacoes = Hash.new {0}
+
+# livros.values.each { |classificacao|
+#   classificacoes[classificacao] += 1
+# }
+
+# puts classificacoes
+
+# bloco
+
+# 5.times { print "Odelay! " }
+
+# 5.times { |vez|
+#   puts vez
+# }
+
+# def em ruby
+
+# def domar( numero_de_megeras )
+#   numero_de_megeras.times {
+#     puts "Megera domada"
+#   }
+# end
+
+# domar 5
+
+
+# def domar( numero_de_megeras )
+#   numero_de_megeras.times {
+#     puts "Megera domada"
+#   }
+#   return numero_de_megeras
+# end
+
+# puts domar(5)
+
+# if and else
+# a = 0
+
+# if a == 100
+#   puts "A expressão é verdadeira, mas agora a é: #{a}"
+# else
+#   puts "#{a} não é igual a 100"
+# end
+
+
+# def fome?(hora_do_dia)
+#   if hora_do_dia < 12
+#     puts "Mim sem fome"
+#     false
+#   else
+#     puts "Mim fome"
+#     true
+#   end
+# end
+
+# def comer_um(o_que)
+#   puts "Mim comer #{o_que}\n"
+# end
+
+# comer_um 'maçã' if fome?(14)
+# comer_um 'maçã' if fome?(10)
